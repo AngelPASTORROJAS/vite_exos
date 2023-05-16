7bea71eaaaa0f4a8e6d02d064d775ace81476003
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-16 13:51:37 +0200

    feat: suppression du composant Horloge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 import Number from './components/Number'
 import Clock from './components/Clock'
-import Horloge from './components/Horloge'
 
 function App() {
   return (
     <>
     <Number></Number>
     <Clock></Clock>
-    <Horloge></Horloge>
     </>
   )
 }
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
 
+/**
+ * The Clock function returns a React component that displays the current time and updates every second
+ * using the useState and useEffect hooks.
+ * @returns A React component that displays the current time in the format of hours, minutes, and
+ * seconds.
+ */
 function Clock() {
   const [time, setTime] = useState(new Date());
 
