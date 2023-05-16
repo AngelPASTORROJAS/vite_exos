c05f2be595818547a822bc14d1837da98b49210a
Angel Daniel PASTOR ROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-16 15:00:57 +0200

    feat: affichage de images toutes les 3s

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,23 +1,28 @@
-import bird from './../assets/img/blackbird.jpg'
-import butterfly from './../assets/img/butterfly.jpg';
-import flowers from './../assets/img/flowers.jpg';
-import nature from './../assets/img/nature.jpg';
-
-const images = [
-    { src: bird, name: 'bird'},
-    { src: butterfly, name: 'butterfly'},
-    { src: flowers, name: 'flowers'},
-    { src: nature, name: 'nature'},
-]
+import bird from "./../assets/img/blackbird.jpg";
+import butterfly from "./../assets/img/butterfly.jpg";
+import flowers from "./../assets/img/flowers.jpg";
+import nature from "./../assets/img/nature.jpg";
+import { useState, useEffect } from "react";
 
 const Images = () => {
-    return <>
-        <ul>
-            {images.map( image =>
-            <li key={image}><img src={image.src} alt={image.name} /></li>
-            )}
-        </ul>
-    </>
-}
+  const images = [
+    { src: bird, name: "bird" },
+    { src: butterfly, name: "butterfly" },
+    { src: flowers, name: "flowers" },
+    { src: nature, name: "nature" },
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
 
-export default Images
+    return () => clearInterval(intervalId);
+  }, [images.length]);
+
+  return (
+      <img sizes="10px" src={images[currentIndex].src} alt={images[currentIndex].name} />
+  );
+};
+
+export default Images;
